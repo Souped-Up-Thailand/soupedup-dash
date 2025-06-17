e622778729ec99f38b09e0b811d1cba302ee4665
Teerut26 <teerutsr@gmail.com>
2025-06-18 00:06:47 +0700

    Refactor authentication configuration to use Google provider and simplify session handling

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,56 +1,37 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import { type NextAuthConfig } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "@/server/db";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
-declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
-  }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
-}
-
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authConfig = {
   providers: [
-    DiscordProvider,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    GoogleProvider({
+      clientId: "",
+      clientSecret: "",
+    }),
   ],
-  adapter: PrismaAdapter(db),
   callbacks: {
     session: ({ session, user }) => ({
       ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
     }),
+    signIn: async ({ user, account, profile }) => {
+      if (account?.provider === "google") {
+        const userExists = await db.user.findUnique({
+          where: { email: user?.email ?? "" },
+        });
+        if (!userExists) {
+          return false;
+        }
+        await db.user.update({
+          where: { email: user.email ?? "" },
+          data: {
+            name: user.name,
+            email: user.email,
+            image: user.image,
+          },
+        });
+      }
+      return true;
+    },
   },
 } satisfies NextAuthConfig;
